d3f745695913663a9b4a5896e3746dd9da0725fd
Tonio Serna <antonelo@gmail.com>
2018-02-08 19:07:24 +0100

    Master/Hotfix * permit user extra params for devise with sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name
+                                                         last_name
+                                                         username
+                                                         picture
+                                                         ])
+    # contact_preference, shared_screen_tool, fluent_language
+
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name
+                                                                last_name
+                                                                username
+                                                                picture
+                                                                ])
+    # contact_preference, shared_screen_tool, fluent_language
+  end
 end
