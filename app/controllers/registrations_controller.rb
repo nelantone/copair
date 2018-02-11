1314f7d185fa3b72e51493b61ea59fb0331dcee2
Tonio Serna <antonelo@gmail.com>
2018-02-11 21:25:34 +0100

    (Feature/Add_TDD_to_confirmed_form) 2. edit account without providing again password *
    - Feature for now to probably change again

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(_resource)
     '/static_pages/thank_you_unconfirmed'
   end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
