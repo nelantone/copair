39a2d79105d83120592c6c7fc79b69978057f15d
Tonio Serna <antonelo@gmail.com>
2018-02-11 20:05:34 +0100

    (Feature/Add_FE_from_edition_and_views_and_remove_unnecesary_code) 2. cleaning up II

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   get 'user/:id' => 'users#show', :as => 'user'
   get 'users' => 'users#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#home'
   post 'static_pages/thank_you_contact' => 'static_pages#thank_you_contact'
   get 'static_pages/thank_you_unconfirmed' => 'static_pages#thank_you_unconfirmed'
