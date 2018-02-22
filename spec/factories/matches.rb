599b2fb33427edc54d2299e5d2c67b8e5a1572a3
Tonio Serna <antonelo@gmail.com>
2018-02-22 15:44:40 +0100

    F/Add_HTM_User-Skill-Relation | 1. Create Match  model class that represents this relationship

---
+++ spec/factories/matches.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :match do
+    
+  end
+end
--- spec/factories/skills.rb
+++ spec/factories/skills.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     present_skills 'Ruby L:5, Rails L:5, Git L:5, CSS L:5, HTML L:5, Linux L:5, JavaScript L:5 BrainFuck L:5'
     future_skills 'React, Terraform, Elixir, Phoenix, Python'
     average_skill_level '5'
-    present_goal 'Lear Elixir Basics'
+    present_goal 'Learn Elixir Basics'
   end
 end
