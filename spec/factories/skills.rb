4aeb145a85d438de7865fa174791fa7c7b14d4f4
Tonio Serna <antonelo@gmail.com>
2018-02-25 23:32:07 +0100

    F/Add_HTM_User-Skill-Relation | 6. Add hash/ary attr via TDD & Virtus

--- spec/factories/skills.rb
+++ spec/factories/skills.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :skill do
-    name_and_level 'Ruby L:5, Rails L:5, Git L:5, CSS L:5, HTML L:5, Linux L:5, JavaScript L:5 BrainFuck L:5'
-    interest 'React, Terraform, Elixir, Phoenix, Python'
+    name_and_level { { ruby: 5.0, rails: 5.0, git: 5.0, css: 5.0, html: 5.0, linux: 5.0, js: 5.0 } }
+    interest       %w[react terraform elixir phoenix python]
   end
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Skill, type: :model do
   let(:skill) { build(:skill) }
 
   it { should have_many(:users).through(:matches) }
+
+  describe 'attributes' do
+    it 'has a String name attribute' do
+      expect(described_class).to have_attribute(:name_and_level).of_type(Hash)
+    end
+
+    it 'has String key and Float value type' do
+      expect(skill.name_and_level).to match a_hash_including(ruby: 5.0, rails: 5.0, git: 5.0, css: 5.0, html: 5.0, linux: 5.0, js: 5.0)
+    end
+  end
 end
